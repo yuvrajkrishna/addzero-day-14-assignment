737e9a0f877939d919b39ea9d8677060b2be0765
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-24 18:27:44 +0530

    alternative array

--- alternativeArray.java
+++ alternativeArray.java
@@ -2,18 +2,28 @@ import java.util.Arrays;
 
 public class alternativeArray {
     public static void main(String[] args) {
-        int arr [] = {10,20,30,40,50};
-        int m = arr.length;
-        int arr2 [] = {60,70,80,90,100};
-        int n = arr2.length;
-        int arr3 [] = new int [m+n];
-        int j = 0 ;
-        int min = Math.min(m,n);
-        for(int i = 0 ; i <min ; i++){
+
+        int arr[] = {10,20,30,40,50};
+        int arr2[] = {60,70};
+
+        int[] arr3 = new int[arr.length + arr2.length];
+
+        int i = 0, j = 0;
+
+        while (i < arr.length && i < arr2.length) {
             arr3[j++] = arr[i];
-            
             arr3[j++] = arr2[i];
+            i++;
         }
+
+        while (i < arr.length) {
+            arr3[j++] = arr[i++];
+        }
+
+        while (i < arr2.length) {
+            arr3[j++] = arr2[i++];
+        }
+
         System.out.println(Arrays.toString(arr3));
     }
 }
