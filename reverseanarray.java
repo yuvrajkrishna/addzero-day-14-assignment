15bde9bd18143524ed48c2a8e59956e8cee9b7c3
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-24 17:58:38 +0530

    reverse an array

---
+++ reverseanarray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class reverseanarray {
+    public static void main(String[] args) {
+        int arr [] = {10,20,30,40,50};
+        int start = 0 ;
+        int end = arr.length - 1 ;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp ;
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
