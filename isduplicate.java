f13d31f564fafd2725799a3e359959850ed18570
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-25 11:25:28 +0530

    is duplicate complete

--- isduplicate.java
+++ isduplicate.java
@@ -1,13 +1,30 @@
 public class isduplicate {
     public static void main(String[] args) {
-        int arr [] = { 1,2,3,2,5,6,7};
-        for(int i = 0 ; i < arr.length ; i++){
-            for(int j = i+1 ; j < arr.length ; j++){
-                if(arr[i] == arr[j]){
-                    System.out.println(arr[i] + "is duplicate");
+
+        int arr[] = {1,2,3,2,5,6,7,2,3,3};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // Check if arr[i] already appeared before
+            boolean alreadyPrinted = false;
+
+            for (int k = 0; k < i; k++) {
+                if (arr[i] == arr[k]) {
+                    alreadyPrinted = true;
+                    break;
                 }
             }
 
+            if (alreadyPrinted)
+                continue;
+
+            // Check for duplicate after i
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i] + " is duplicate");
+                    break;   // Print only once
+                }
+            }
         }
     }
 }
